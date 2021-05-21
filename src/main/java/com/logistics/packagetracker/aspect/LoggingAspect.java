0911a2a8e809d31820ba0876c9581c04863b1f35
Ngo <ngomalalibo@yahoo.com>
2021-05-21 18:03:22 +0100

    Documentation

--- src/main/java/com/logistics/packagetracker/aspect/LoggingAspect.java
+++ src/main/java/com/logistics/packagetracker/aspect/LoggingAspect.java
@@ -1,7 +1,6 @@
 package com.logistics.packagetracker.aspect;
 
 import com.logistics.packagetracker.entity.Package;
-import com.logistics.packagetracker.enumeration.PackageStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
@@ -14,21 +13,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class LoggingAspect
 {
-    /** This aspect can be used to implement cross cutting concerns like security, logging, caching, emailing etc. The advice is executed along with the Join Point. */
+    /**
+     * This aspect can be used to implement cross cutting concerns like security, logging, caching, emailing etc. The advice is executed along with the Join Point.
+     */
     
     @AfterReturning(value = "@annotation(Loggable)", returning = "returnValue")
     public void logPackageMethodCall(JoinPoint joinPoint, Object returnValue)
     {
         Package pack = (Package) returnValue;
         logTracker(joinPoint, pack);
-        log.info("Logged tracker details via aspect");
+        // log.info("Logged tracker details via aspect");
     }
     
     @AfterThrowing(value = "@annotation(Loggable)", throwing = "exception")
     public void logPackageMethodException(JoinPoint joinPoint, Throwable exception)
     {
         log.info("Cause: {}, Message: {}", exception.getCause(), exception.getMessage());
-        log.info("Logged via aspect after throwing");
+        // log.info("Logged via aspect after throwing");
     }
     
     private void logTracker(JoinPoint joinPoint, Package pack)
